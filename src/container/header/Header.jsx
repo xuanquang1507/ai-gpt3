86651c65d1d344038a5e22ec9ea0892b7e1f19c5
xuanquang1507 <xuanquang15072002aa@gmail.com>
2023-10-03 17:28:59 +0700

    commit

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='home' >Header</div>
-  )
-}
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
