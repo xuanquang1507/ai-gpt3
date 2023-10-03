f39b1269079a5323811bbb0dc97d9ecc2d6fc2a1
xuanquang1507 <91023380+xuanquang1507@users.noreply.github.com>
2023-10-03 11:51:44 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import "./navbar.css";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/GPT-3.svg";
 
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
-        <div className="gpt3__navbar-links">
-            <div className="gpt3__navbar-links_logo">
-                {/* <img src={logo} alt="logo"/> */}
-            </div>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
         </div>
-  </div>
-  )
-  
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">Case Studies</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
